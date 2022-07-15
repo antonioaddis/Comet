839dd28ee6c6de7fa52740e85d84d5363034f80a
Antonio Addis <antonio.addis@inaf.it>
2022-07-15 14:56:11 +0200

    change test_voevents

--- comet/plugins/Voevent.py
+++ comet/plugins/Voevent.py
@@ -17,9 +17,8 @@ from astropy.coordinates import SkyCoord
 from comet.utility.xml import xml_document
 from ligo.skymap.io.fits import read_sky_map
 from ligo.skymap.postprocess.contour import contour as ligo_contour
-from comet.testutils import DUMMY_VOEVENT_GCN, DUMMY_VOEVENT_GCN_FERMI, DUMMY_VOEVENT_INTEGRAL, \
-    DUMMY_VOEVENT_CHIME, DUMMY_VOEVENT_LIGO, DUMMY_VOEVENT_LIGO_PRELIMINARY, DUMMY_VOEVENT_LIGO_INITIAL, \
-    DUMMY_VOEVENT_GCN_FERMI \
+from comet.plugins.test.test_voevents import DUMMY_VOEVENT_GCN, DUMMY_VOEVENT_INTEGRAL, DUMMY_VOEVENT_CHIME, \
+    DUMMY_VOEVENT_LIGO, DUMMY_VOEVENT_LIGO_INITIAL, DUMMY_VOEVENT_LIGO_PRELIMINARY, DUMMY_VOEVENT_GCN_FERMI
 
 class DummyEvent(object):
     """
